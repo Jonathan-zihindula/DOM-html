2401dc9deeec4dc667545964ce7dcf5e7c0498f9
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-03-01 13:19:03 +0200

    DOM html

---
+++ javascript/javascript.js
@@ -0,0 +1,9 @@
+var titredepage = document.title;
+var page = document.body;
+var lien = document.links;
+
+alert('contenue de titre page : ' + titredepage + 
+        '\ncontenue de notre page :  ' + page +
+        '\ncontenue de lien : ' + lien[0] );
+
+        // chapiter 35 , suite 36
