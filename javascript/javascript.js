8455623b95a00d83f1c7cc1c6598d30b0ed94c0c
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-03-01 14:05:24 +0200

    using of replaceChild() proprety

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,7 @@
 var title = document.getElementById('gros_titre');
 var parent = document.body;
+var newTitle = document.createElement('h2');
+newTitle.id = 'medium_title';
+newTitle.innerHTML = 'title amended in JS';
 
-parent.removeChild(title);
+parent.replaceChild(newTitle,title);
