0dd37128c910799be946e44244a30ad7f48b9c42
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-03-04 14:57:24 +0200

    addEventListener

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,28 @@
+var p1 = document.querySelector('p');
 
- var title = document.getElementById('gros_titre');
+p1.addEventListener('mouseover',Function1);
+p1.addEventListener('mousedown',Function2);
+p1.addEventListener('mouseout',Reset1);
+p1.addEventListener('mouseup',Reset2);
 
- var div = title.nextSibling.nextSibling;
- div.innerHTML = '<p>div all amended</p>';
-//  event in JS
+function Function1(){
+    this.innerHTML = "click me now";
+    this.style.backgroundColor = "orange";
+}
+function Function2(){
+    this.innerHTML = "congratilation";
+    this.style.color = "#26C";
+    this.style.fontWeight = 'bold';
+    this.style.fontSize = '24px';
+    
+}
+function Reset1(){
+    this.innerHTML = "click me ";
+    this.style.backgroundColor = "";
+}
+function Reset2(){
+    this.innerHTML = 'pass on me ';
+    this.style.color = '';
+    this.style.fontWeight = '';
+    this.style.fontSize = '';
+}
