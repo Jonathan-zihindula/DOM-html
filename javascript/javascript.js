85adcce11dc6a5c1f805a5c0b09ae0e752f5c1b8
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-03-05 14:07:14 +0200

    end event

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,24 +1,12 @@
 var div1 = document.getElementById('div1');
 var p1 = document.getElementById('p1');
-var body = document.body;
-var strong = document.querySelector('strong');
-p1.addEventListener('click',CP1,true);
-strong.addEventListener('click',CS,true);
 
-function CP1 (){
-    alert('capt paragraphe');
-}
-function CS (){
-    alert('capt strong');
+div1.addEventListener('click',textD,true);
+p1.addEventListener('click',textP,true);
+function textD(e){
+  e.priventDefault();
 }
 
-div1.addEventListener('click',BD1);
-body.addEventListener('click',BB);
-
-function BD1 (){
-    alert('bouillonnement div')
-}
-
-function BB (){
-    alert('bouillonnement body');
+function textP(){
+    alert('la methode privent ne declenche pas la propagation ');
 }
