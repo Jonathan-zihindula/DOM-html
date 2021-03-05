56a8d36e38a1e7040c92482a003980c26b8cecf1
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-03-05 17:07:08 +0200

    location object

--- javascript/BOM/javascript.js
+++ javascript/BOM/javascript.js
@@ -1,15 +1,23 @@
-var para = document.querySelector('p');
-var loc = navigator.geolocation;
-(function(){
-    if (loc){
-        loc.getCurrentPosition(coordonnes);
-    }
-    else {
-        para.innerHTML = 'localisation non disponible ';
-    }
-})();
-function coordonnes(x){
-    para.innerHTML = 
-    'latitude: ' + x.coords.latitude +
-    '<br>Longitude : ' + x.coords.longitude;
+var para = document.querySelectorAll('p');
+var charger = document.getElementById('charger');
+var recharger = document.getElementById('recharger');
+var changer = document.getElementById('changer');
+
+charger.addEventListener('click',charge);
+recharger.addEventListener('click',recharge);
+changer.addEventListener('click',change);
+function charge (){
+    location.assign('http://pierre-giraud.com');
 }
+function recharge (){
+    location.reload();
+}
+function change (){
+    location.replace('http://pierre-giraud.com');
+}
+
+var url = location.href;
+var chemin = location.pathname;
+
+para[0].innerHTML = 'url : ' + url + 
+    '<br>chemin : ' + chemin ;
